dc88310aed3655d62f4326ce7b97ab8dbcf8901d
Lbenedettomt <luigibenedettomt@gmail.com>
2019-12-04 14:24:51 -0300

    Final modifications

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 import styled from 'styled-components';
-import roadImage from '../assets/roadImage.jpg';
+import boatImage from '../assets/boatImage.jpg';
 
-const Styles = styled.div``;
+const Styles = styled.div`
+  .jumbo {
+    background: url(${boatImage}) no-repeat fixed bottom;
+    background-size: cover;
+    color: #efefef;
+    height: 200px;
+    position: relative;
+    z-index: -1;
+  }
+  .overlay {
+    background-color: #000;
+    opacity: 0.6;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
+`;
 
 export const Jumbotron = () => (
-    <Styles>
-        <Jumbo fluid classname="jumbo">
-            <div classname="overlay"></div>
-            <Container>
-                <h1>Code Life</h1>
-                <p>Aprenda a codar pelo meu site!</p>
-            </Container>
-        </Jumbo>
-    </Styles>
+  <Styles>
+    <Jumbo fluid className="jumbo">
+      <div className="overlay"></div>
+      <Container>
+        <h1>Welcome</h1>
+        <p>Learn to code from my YouTube videos</p>
+      </Container>
+    </Jumbo>
+  </Styles>
 )
